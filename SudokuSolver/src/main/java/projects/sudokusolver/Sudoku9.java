b337226e61cfa11ef6ab301a101e3aa5e2d7dcd4
Uli Köhler <ulikoehler@online.de>
2011-12-16 20:13:05 +0100

    Added code

--- SudokuSolver/src/main/java/projects/sudokusolver/Sudoku9.java
+++ SudokuSolver/src/main/java/projects/sudokusolver/Sudoku9.java
@@ -1,26 +1,49 @@
 package projects.sudokusolver;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 /**
- *
+ * <b>Definitions:</b>
+ * Alphabet: A collection of characters representing the 
  * @author uli
  */
 public class Sudoku9 {
 
     private static final char undefined = 'x';
     private String alphabet = "123456789";
-    private SudokuField[][] fields = new SudokuField[9][];
+    private SudokuField[][] fields = new SudokuField[9][]; //Rows first
 
     public class SudokuField {
 
+        public int getContent() {
+            return content;
+        }
         private int content = -1; //As index of the alphabet
-        private boolean[] canContainElement = new boolean[alphabet.length()];
+        private boolean[] possibilities = new boolean[alphabet.length()];
+
+        public void setPossibility(int index, boolean b) {
+            possibilities[index] = b;
+        }
+
+        public boolean getPossibility(int index) {
+            return possibilities[index];
+        }
+
+        /**
+         * @return The number of current possibilities begin true
+         */
+        public int getNumPossibilities() {
+            int ctr = 0;
+            for (boolean possibility : possibilities) {
+                if (possibility) {
+                    ctr++;
+                }
+            }
+            return ctr;
+        }
 
         /**
          * Initializes a Sudoku field with a specific content
@@ -28,8 +51,8 @@ public class Sudoku9 {
          */
         public SudokuField(int content) {
             this.content = content;
-            for (int i = 0; i < canContainElement.length; i++) {
-                canContainElement[i] = false;
+            for (int i = 0; i < possibilities.length; i++) {
+                possibilities[i] = false;
             }
         }
 
@@ -37,20 +60,20 @@ public class Sudoku9 {
          * Constructs a new SudokuField instance 
          */
         public SudokuField() {
-            for (int i = 0; i < canContainElement.length; i++) {
-                canContainElement[i] = true;
+            for (int i = 0; i < possibilities.length; i++) {
+                possibilities[i] = true;
             }
         }
 
         @Override
         public String toString() {
-            return "SudokuField{" + "content=" + content + ", canContainElement=" + canContainElement + '}';
+            return "SudokuField{" + "content=" + content + ", canContainElement=" + possibilities + '}';
         }
     }
 
-    public Sudoku9(File file) {
+    public Sudoku9(InputStream input) {
         try {
-            List<String> lines = FileUtils.readLines(file);
+            List<String> lines = IOUtils.readLines(input);
             if (lines.size() != 9) {
                 throw new IllegalArgumentException("The given file does not contain 9 lines");
             }
@@ -77,8 +100,69 @@ public class Sudoku9 {
                 }
             }
         } catch (IOException ex) {
-            Logger.getLogger(Sudoku9.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex);
         }
 
+    }
+
+    public String getAlphabet() {
+        return alphabet;
+    }
+
+    public SudokuField[][] getFields() {
+        return fields;
+    }
+
+    /**
+     * Getter for a specific field
+     * @param row The zero-based row
+     * @param col The zero-based column
+     * @return The specific field
+     */
+    public SudokuField getField(int row, int col) {
+        return fields[row][col];
+    }
+
+    /**
+     * Getter for a specific row
+     * @param index The zero-bazed index
+     * @return The row as array
+     */
+    public SudokuField[] getRow(int index) {
+        return fields[index];
+    }
+
+    /**
+     * Getter for a specific column
+     * @param index The zero-based index
+     * @return The column as array
+     */
+    public SudokuField[] getColumn(int index) {
+        SudokuField[] ret = new SudokuField[9];
+        for (int i = 0; i < 9; i++) {
+            ret[i] = fields[i][index];
+        }
+        return ret;
+    }
+
+    /**
+     * Getter for a specific subfield
+     * @param index The zero-based index
+     * @return The subfield as array
+     */
+    public SudokuField[] getSubfield(int index) {
+        SudokuField[] ret = new SudokuField[9];
+        //Calculate the left uppermost field in the 
+        int dividend = index / 3;
+        int remainder = index % 3;
+        int startRow = 3 * dividend;
+        int startCol = 3 * remainder;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                SudokuField field = fields[startRow + i][startCol + j];
+                ret[(3 * i) + j] = field;
+            }
+        }
+        return ret;
     }
 }
--- SudokuSolver/src/main/java/projects/sudokusolver/Sudoku9Solver.java
+++ SudokuSolver/src/main/java/projects/sudokusolver/Sudoku9Solver.java
@@ -1,11 +1,65 @@
 package projects.sudokusolver;
 
+import com.google.common.collect.Sets;
+import java.io.InputStream;
+import java.util.Set;
+import projects.sudokusolver.Sudoku9.SudokuField;
+
 /**
  *
  * @author uli
  */
 public class Sudoku9Solver {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws Exception {
+        InputStream in = Sudoku9Solver.class.getResourceAsStream("/wikipedia.sud");
+        Sudoku9 sudoku = new Sudoku9(in);
+        calculatePossibilities(sudoku);
+    }
+
+    private static void calculatePossibilities(Sudoku9 sudoku) {
+        //Rows
+        for (int i = 0; i < 9; i++) {
+            SudokuField[] row = sudoku.getRow(i);
+            System.out.println("Row " + (i + 1));
+            updatePossibilities(row);
+        }
+        //Column
+        for (int i = 0; i < 9; i++) {
+            SudokuField[] col = sudoku.getColumn(i);
+            System.out.println("Col " + (i + 1));
+            updatePossibilities(col);
+        }
+        //Subfield
+        for (int i = 0; i < 9; i++) {
+            SudokuField[] subfield = sudoku.getSubfield(i);
+            System.out.println("Subfield " + (i + 1));
+            updatePossibilities(subfield);
+        }
+    }
+
+    /**
+     * For a specific set of correlated fields, removes possibilities of numbers
+     * that already occur in the correlated field
+     * @param fields 
+     */
+    private static void updatePossibilities(SudokuField[] fields) {
+        Set<Integer> setInRow = Sets.newTreeSet(); //All numbers (indices in alphabet) which are contained
+        //Find all numbers being set in the row
+        for (SudokuField field : fields) {
+            if (field.getContent() != -1) {
+                setInRow.add(field.getContent());
+            }
+        }
+        //Remove the possibility of the found numbers from all fields
+        for (int i = 0; i < fields.length; i++) {
+            SudokuField field = fields[i];
+            for (int possibilityToBeRemoved : setInRow) {
+                if (field.getContent() == -1) { //No need to set possibilities for fields already having content
+                    field.setPossibility(possibilityToBeRemoved, false);
+                    System.out.println("    Removing possibility for " + (possibilityToBeRemoved + 1) + " at index " + (i + 1));
+                }
+            }
+        }
     }
 }
